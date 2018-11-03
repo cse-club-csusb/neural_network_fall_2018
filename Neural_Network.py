ce4d34f07be6fe264ed3169375eb31d42b67c106
Sagar Patel <sagarp@coyote.csusb.edu>
2018-11-02 19:32:08 -0700

    Created Neural Network Class for everyone to work on

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,1 +1,30 @@
+_author = ["Sagar Patel", "Quimpie Tuada", "Eugene Kim", "Andrew Beechko",
+           "Adrian Insingo", "Garrett Gant", "Joseph Premdas", "Tyler Oviatt" ,"Jorge Martinez",
+           "Manuel Gavino", "Chris Magnuson", "Mike Chang Godinez", "Ethan Jones",
+           "Zion Jones", "Colin Morris", "Mario Victorino" ]
 import numpy as np
+import pandas
+
+class Neural_Network:
+    def __init__(self):
+        print("This is a Neural Network")
+        
+    def forward(self, x):
+        o = 10
+        return o
+    
+    def sigmoid(self, s):
+        return s
+
+    def sigmoidPrime(self, s):
+        return s
+    def backward(self,X,y,o):
+        print("nothing to return here")
+
+    def train(self, X, y):
+        print("nothing to return here")
+
+    def saveWeights(self):
+        print("nothing to return here")
+    def predict(self):
+        
