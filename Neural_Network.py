ed4346d1fea2bc087884441d804b9258607cb87f
r0meroh <romeroh862@gmail.com>
2019-02-05 11:49:11 -0800

    provided an alternative to the neural network dunder innit definition. Also, communicated reasoning behind two seperate paths the network object could potentially take. That will be determined until the latter stages of the project

--- Neural_Network.py
+++ Neural_Network.py
@@ -3,17 +3,31 @@ import numpy as np
 import pandas
 
 # this training data consist of three int RGB values
-training_data = ([233,100,199],[133,110,211],[201,155,99])
+training_data = np.array({[233,100,199],[133,110,211],[201,155,99]})
 # the expected results only changes the last two values by dropping the left digit
-expected_results = ([233,00,99],[133,10,11],[201,55,9])
+expected_results = np.array ([[233,00,99],[133,10,11],[201,55,9]])
 
 
 
 # test
 class Neural_Network:
-    def __init__(self):
+    def __init__(self): # we need number of input nodes in this parameter[suggested number is 3]
+                        # after that parameter we need another specifying the number of output nodes
+                        # lastly the 'learning rate'
+                        # example:
+                        # def __init__(self, in_nodes, out_nodes, learning_rate):
+                           # self.in_nodes = 3
+                           # self.out_nodes = 3
+                           # self.learning_rate = .1
+
+                        # I still need to research how the 'hidden nodes' are to be implemented
+                        # also, we won't know how the network input nodes will handle an input consisting
+                        # of three int values per input(the RGB values)
+                        # we could, in theory, split each single RGB value individually into one per node
+                        # but we run the risk of the network not recognizing the correlation with ints & RGB
         print("This is a Neural Network")
-        
+
+
     def forward(self, x):
         o = 10
         return o
