dd99f84f087047820996bd44e88c5f11cb99b154
HR1777306 <romerohugo77@yahoo.com>
2019-02-12 23:37:54 -0800

    normalized the original input data and "expected results" data. The original values are commented directly above to facilitate the readability and purpose of the normalization.

--- Neural_Network.py
+++ Neural_Network.py
@@ -3,9 +3,14 @@ import numpy as np
 import pandas
 
 # this training data consist of three int RGB values
-training_data = np.array(([233,100,199],[133,110,211],[201,155,99]), dtype=float)
+# the training data has to be normalized in order for the network to work and self adjust easily
+# this is done by the equation z = (x-xmin)/xmin
+# that means our new training data will all fall between zero and one like expected after normalization
+# training_data = np.array(([233,100,199],  [133,110,211],    [201,155,99]), dtype=float) original values, new are below
+training_data = np.array(([0.82,-0.22,0.55],[0.04,-0.14,0.65],[0.57,0.21,-0.23]),dtype=float)
 # the expected results only changes the last two values by dropping the left digit
-expected_results = np.array ([[233,00,99],[133,10,11],[201,55,9]],dtype=float)
+# expected_results = np.array ([[233,00,99],[133,10,11],       [201,55,9]],dtype=float) original values, new values are below
+expected_results = np.array(([0.82,-1,0.55],[0.04,-0.92,-0.91],[0.57,-0.57,-0.93]),dtype=float)
 
 np.random.seed(1)
 
@@ -13,10 +18,10 @@ np.random.seed(1)
 # test
 class Neural_Network(object):
     def __init__(self):
-         self.inputsize  = 3             # number of neurons is choosen by
-         self.outputsize = 1             # Nh =       Ns
-         self.hidden_size = 1            #      ______________
-                                         #     (a * (Ni + No))  a = scaling factor h = hidden, i = input, o = output, s = samples in data set
+         self.input_layer  = 3             # number of neurons is choosen by
+         self.middle_layer = 4             # Nh =       Ns
+         # self.hidden_size = 1            #      ______________
+         self.output_layer = 2             #     (a * (Ni + No))  a = scaling factor h = hidden, i = input, o = output, s = samples in data set
          print("This is a Neural Network")
 
 
